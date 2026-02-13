3ff22c8598760d408cdd2e88492498cf9978359b
dhir4j <kapsedhiraj15@gmail.com>
2026-02-14 00:19:05 +0530

    updated svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/forsvg.svg" type="image/svg+xml" />
+      </head>
       <body className="antialiased">
         <Loader />
         {!hideChrome && <Navbar />}
